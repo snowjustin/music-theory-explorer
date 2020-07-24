b7ca9fc7ed3487b696bd64889e1167ef37e9667c
Justin Snow <snowjustin@gmail.com>
2020-07-24 14:59:29 +0200

    adding new functionality to display the note on the circle

--- lib/note.lua
+++ lib/note.lua
@@ -1,18 +1,34 @@
 -- A circle that displays the note we are inspecting as a circle
 
+
+-- Some constants about notes
+NOTE_RADIUS = 40
+NOTE_A = "A"
+NOTE_B = "B"
+NOTE_C = "C"
+NOTE_D = "D"
+NOTE_E = "E"
+NOTE_F = "F"
+NOTE_G = "G"
+NOTE_SHARP = "#"
+NOTE_FLAT = "b"
+
 Note = Object:extend()
 
-function Note:new(x, y)
+function Note:new(x, y, text)
     -- circle settings
     self.x = x
     self.y = y
-    self.radius = 10
+    self.radius = NOTE_RADIUS
     self.shrink = false
+    local font = love.graphics.getFont()
+    self.color = {0, 0, 0}
+    self.text = love.graphics.newText(font, {self.color, text})
 end
 
 function Note:update(dt)
-    local max_radius = 50
-    local min_radius = 10
+    local max_radius = NOTE_RADIUS
+    local min_radius = 25
 
     if self.radius >= max_radius then
         self.shrink = true
@@ -22,12 +38,15 @@ function Note:update(dt)
 
 
     if self.shrink then
-        self.radius = self.radius - 10 * dt
+        self.radius = self.radius - 8 * dt
     else
-        self.radius = self.radius + 10 * dt
+        self.radius = self.radius + 8 * dt
     end
 end
 
 function Note:draw()
+    local textWidth = self.text:getWidth()
+    local textHeight = self.text:getHeight()
     love.graphics.circle("fill", self.x, self.y, self.radius)
+    love.graphics.draw(self.text, self.x, self.y, 0, 1, 1, textWidth / 2, textHeight / 2)
 end
--- main.lua
+++ main.lua
@@ -1,14 +1,38 @@
 function love.load()
     Object = require "lib.classic"
     require "lib.note"
+    
+    
+    -- Place 8 notes in random places on the screen
+    MAX_WIDTH = love.graphics.getWidth() - 50
+    MAX_HEIGHT = love.graphics.getHeight() - 50
+    MIN_WIDTH = 50
+    MIN_HEIGHT = 50
 
-    note = Note(100, 100)
+    notes = {
+        Note(MAX_WIDTH / 4, MAX_HEIGHT / 4, NOTE_A),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+        -- Note(),
+    }
 end
 
 function love.update(dt)
-    note:update(dt)
+    for i=1, #notes do
+        notes[i]:update(dt)
+    end
 end
 
 function love.draw()
-    note:draw()
+    for i=1, #notes do
+        notes[i]:draw()
+    end
 end
